a0adfacb887ed404a3c5e0ab31656e32ab0c7cd2
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-07-19 11:45:03 +0200

    Added style and animations for empty bank accounts screen

--- client/src/app/components/main/account/accounts/accounts.component.ts
+++ client/src/app/components/main/account/accounts/accounts.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { slideInDownAnimation } from 'src/app/animations';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
-  styleUrls: ['./accounts.component.scss']
+  styleUrls: ['./accounts.component.scss'],
+  animations: [ slideInDownAnimation ]
 })
 export class AccountsComponent implements OnInit {
+
+  @HostBinding('@routeAnimation') routeAnimation = true;
+  @HostBinding('style.display') display = 'block';
+  @HostBinding('style.position') position = 'relative';
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
